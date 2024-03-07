64f8e8b0ab5eafb6213d5bd1beb218ff4ef55e62
JairLopezLopez <127882744+JairLopezLopez@users.noreply.github.com>
2024-03-06 20:35:44 -0600

    Update reloj.py
    Se modifico el código para que aceptara la hora 00:00, además se corrigió el error donde no mencionaba los minutos primero para indicar cuantos habían transcurrido desde la hora

--- reloj.py
+++ reloj.py
@@ -1,4 +1,3 @@
-
 hora_ingresada = input("Ingresa la hora en formato 5:30: ")
 
 def convertir_hora_a_palabras(hora):
@@ -9,12 +8,11 @@ def convertir_hora_a_palabras(hora):
     
     palabras = ["", "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez",
                 "Once", "Doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho",
-                "diecinueve", "veinte", "veintiuno", "veintidos", "veintitres", "veinticuatro"]
+                "diecinueve", "veinte", "veintiuno", "veintidos", "veintitres", "veinticuatro", "Veinticinco", "veintiseis", "veintisiete", "veintiocho"]
     
-    for i in range(21, 30):
-        palabras.append(palabras[i - 20])
-
-    if minutos_entero == 0:
+    if hora_entero == 0 and minutos_entero == 0:
+        return "Media noche"
+    elif minutos_entero == 0:
         return f"{palabras[hora_entero]} en punto"
     elif minutos_entero == 15:
         return f"{palabras[hora_entero]} y cuarto"
@@ -22,24 +20,14 @@ def convertir_hora_a_palabras(hora):
         return f"Media después de las {palabras[hora_entero]}"
     elif minutos_entero == 45:
         return f"Cuarto para las {palabras[hora_entero + 1]}"
+    elif minutos_entero == 1:
+        return f"{palabras[minutos_entero]} minuto después de las {palabras[hora_entero]}"
+    elif minutos_entero == 5:
+        return f"{palabras[minutos_entero]} minutos después de las {palabras[hora_entero]}"
     elif minutos_entero < 30:
-        if minutos_entero == 1:
-            return f"Un minuto después de las {palabras[hora_entero]}"
-        elif minutos_entero == 5:
-            return f"Cinco minutos después de las {palabras[hora_entero]}"
-        elif minutos_entero > 20:
-            return f"{palabras[hora_entero]} y {palabras[minutos_entero - 20]} minutos después de las {palabras[hora_entero]}"
-        else:
-            return f"{palabras[minutos_entero]} minutos después de las {palabras[hora_entero]}"
+        return f"{palabras[minutos_entero]} minutos después de las {palabras[hora_entero]}"
     else:
-        if minutos_entero == 31:
-            return f"Un minuto para las {palabras[hora_entero + 1]}"
-        elif minutos_entero == 55:
-            return f"Cinco minutos para las {palabras[hora_entero + 1]}"
-        elif minutos_entero > 50:
-            return f"{palabras[60 - minutos_entero]} minutos para las {palabras[hora_entero + 1]}"
-        else:
-            return f"{palabras[60 - minutos_entero]} minutos para las {palabras[hora_entero + 1]}"
+        return f"{palabras[60 - minutos_entero]} minutos para las {palabras[hora_entero + 1]}"
 
 resultado = convertir_hora_a_palabras(hora_ingresada)
 print(resultado)
